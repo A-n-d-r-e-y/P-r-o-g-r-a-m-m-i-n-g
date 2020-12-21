f175d5271b5f006981cfc7db3aa720d072fafa07
A-n-d-r-e-y <58929157+A-n-d-r-e-y@users.noreply.github.com>
2020-12-21 10:49:57 +0700

    Add files via upload

---
+++ Head_tables.h
@@ -0,0 +1,92 @@
+#include <iostream>
+#include "cstring"
+#include <cmath>
+
+using namespace std;
+
+class Table {
+    int* sz; char* color;
+public:
+    Table();
+    Table(int, const char*);
+    Table(int);
+    ~Table();
+    
+    void Show();
+    int CalcVolume();
+};
+
+Table::Table() {
+    cout << "Конструктор Table::Table()" << endl;
+    sz = new int;
+    color = new char[32];
+}
+
+Table::Table(int sz1, const char* color1) {
+    cout << endl << "Конструктор Table::Table(int sz1, char *color1)" << endl;
+    sz = new int;
+    *sz = sz1;
+    color = new char[strlen(color1) + 1];
+    strcpy_s(color, strlen(color1) + 1, color1);
+}
+
+Table::Table(int sz1) {
+    cout << endl << "Конструктор Table::Table(int sz1)" << endl;
+    sz = new int; *sz = sz1;
+    color = new char[32];
+}
+
+void Table::Show() {
+    cout <<  "   Размер = " << *sz << endl << "   Цвет - " << color << endl;
+}
+
+int Table::CalcVolume() {
+    return pow(*sz, 3);
+}
+
+Table::~Table() {
+    cout << endl << "Деструктор Table::~Table() " << endl;
+    delete sz; delete[] color;
+}
+
+
+class CompTable :public Table {
+    int* h; char* matrl;
+public:
+    CompTable();
+    CompTable(int, const char*, int, const char*);
+    ~CompTable();
+    
+    void ShowCT();
+    int CalcVolumeCT();
+};
+
+CompTable::CompTable() :Table() {
+    cout << endl << "--- Конструктор CompTable::CompTable() :Table() " << endl;
+    h = new int; matrl = new char[32];
+
+}
+
+CompTable::CompTable(int sz1, const char* color1, int h1, const char* mtrl1)
+    :Table(sz1, color1) {
+    cout << endl << "--- Конструктор CompTable::CompTable(int sz1, char* color1, int h1, char* mtrl1) :Table(sz1, color1) " << endl;
+    h = new int; *h = h1;
+    matrl = new char[strlen(mtrl1) + 1];
+    strcpy_s(matrl, strlen(mtrl1) + 1, mtrl1);
+}
+
+CompTable::~CompTable() {
+    cout << endl << "--- Деструктор CompTable::~CompTable() " << endl;
+    delete h; delete[] matrl;
+}
+
+void CompTable::ShowCT() {
+    cout <<  "   Высота подставки = " << *h << endl << "   Материал - " << matrl << endl;
+    Table::Show();
+}
+
+int CompTable::CalcVolumeCT() {
+    return pow(*h, 3) + Table::CalcVolume();
+}
+
+
---
+++ lab_6_tables_kulev.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include "Head_tables.h"
+#include <cmath>
+
+using namespace std;
+
+int main(){
+	setlocale(LC_ALL, "Russian");
+	cout << "----Программа реализации иерархии классов столов!----" << endl;
+	
+ 	Table* table = new Table(10, "Серый");
+    cout << endl << "  Характеристики стола: " << endl; table->Show();
+    cout << "    Объём стола - " << table->CalcVolume() << endl;
+
+	CompTable* ctable = new CompTable(10, "белый", 2, "ДВП");
+    cout << endl << "  Характеристики компьютерного стола: " << endl; ctable->ShowCT();
+    cout << "    Объём компьютерного стола - " << ctable->CalcVolumeCT() << endl;
+
+	delete table;
+	table = nullptr;
+
+    delete ctable;
+	ctable = nullptr;
+
+    return 0;
+}
+
